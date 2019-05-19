2849ba56c2d071e7702e215ccda1c9e40bb8a38e
Wanjiku <Wanjiku>
2019-05-19 13:28:00 +0300

    Created a tag model

--- instapics/models.py
+++ instapics/models.py
@@ -1,7 +1,21 @@
 from django.db import models
 
+class tag(models.Model):
+    tag = models.CharField(max_length = 100)
+
+    def __str__(self):
+        return self.tag
+
+        
 class Profile(models.Model):
     first_name = models.CharField(max_length=100)
     last_name = models.CharField(max_length = 100)
     username = models.CharField(max_length = 100)
     email = models.EmailField()
+
+    def __str__(self):
+        return self.username
+
+    class Meta:
+        ordering = ['username']
+
