88253812be4ffd3f7d61f88bb39c270a54fc8659
Wanjiku <Wanjiku>
2019-05-19 14:37:13 +0300

    Edited navbar, removed dropdown menu

--- instagramme/urls.py
+++ instagramme/urls.py
@@ -2,8 +2,8 @@ from django.conf.urls import url,include
 from django.contrib import admin
 
 urlpatterns = [
-    url(r'^admin/', admin.site.urls),
-    url(r'^instapics/',include('instapics.urls')),
+    url(r'^admin/', admin.site.urls,name=admin),
+    url(r'',include('instapics.urls')),
     url(r'^accounts/', include('registration.backends.simple.urls')),
     
 ]
